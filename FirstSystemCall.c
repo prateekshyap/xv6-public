f5c8c402d8a376d5f8d4a7fd6f088ec3d2dd9da6
prateekshyap <prateekshyapriyadarshini@gmail.com>
2021-09-11 18:57:05 +0530

    system call and user program added

---
+++ FirstSystemCall.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void)
+{
+	printf(1,"Note: Covid started in the year %d and ruined our lives\n",getyear());
+	exit();
+}
---
+++ MyFirstProgram.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+	printf(1,"My first xv6 program learnt at IITG\n");
+	exit();
+}
